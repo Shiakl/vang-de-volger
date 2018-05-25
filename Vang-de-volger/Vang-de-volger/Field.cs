87fed44e94ab150ba33f480b7d3be755cbdafbd2
Draevas <s.phan96@hotmail.com>
2018-05-25 10:20:41 +0200

    comments

--- Vang-de-volger/Vang-de-volger/Field.cs
+++ Vang-de-volger/Vang-de-volger/Field.cs
@@ -24,6 +24,7 @@ namespace Vang_de_volger
             _playfield = new Tile[NUM_OF_TILES];            
         }
 
+        //not scallable
         private enum UNITTYPE
         {
             BLOCK,
